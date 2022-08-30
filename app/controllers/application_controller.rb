7e782c5f8da3bc098cc7bbbeac3cdf518cffa22d
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-30 15:07:00 +0300

    commit tree 5c2da8793fb04f084627e7a34a974755c22f10e2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  # set content-type
+  set :default_content_type, 'application/json'
 
+  get '/bakeries' do
+    bakeries = Bakery.all
+
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    # baked_goods = BakedGood.all.order(price: :desc)
+    # baked_goods = BakedGood.all.sort_by(&:price).reverse
+    baked_goods = BakedGood.all.order(:price).reverse
+
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    # baked_goods = BakedGood.all.order(price: :desc).first
+    baked_goods = BakedGood.all.sort_by(&:price).reverse.first
+    # baked_goods = BakedGood.all.order(:price).reverse.first
+
+    baked_goods.to_json
+  end
 end
